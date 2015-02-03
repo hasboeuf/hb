388a876e243a426946518ec3afbf303cbf98ef73
hasboeuf <adrien.gavignet@gmail.com>
2015-02-03 10:38:39 +0100

    HbTools:
    * Class cleaning.
    * Creation of subdirs.

--- network-example/network-user/src/main.cpp
+++ network-example/network-user/src/main.cpp
@@ -15,7 +15,7 @@ int main(int argc, char *argv[])
 	hb::tools::HbApplicationHelper::initApp("hb-company", "hb-company.com");
 	hb::tools::HbApplicationHelper::initSkin("fusion");
 
-    hb::log::HbLogService::processArgs(argc, argv);
+    hb::log::HbLogService::processArgs( argc, argv );
 
     UserMainWindow w;
     w.show();
