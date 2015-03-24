d564e982c54a4e7898065eb7be5dcbe22c9afe10
Adrien Gavignet <adrien.gavignet@gmail.com>
2015-03-24 17:35:57 +0100

    Hb:
    * initApp with real hb-io domain.

--- link-example/src/main.cpp
+++ link-example/src/main.cpp
@@ -12,8 +12,8 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
-    hb::tools::HbApplicationHelper::initApp("hb-company", "hb-company.com");
-    hb::tools::HbApplicationHelper::initSkin("fusion");
+    hb::tools::HbApplicationHelper::initApp( "hb-io", "hb-io.com" );
+    hb::tools::HbApplicationHelper::initSkin( "fusion" );
     hb::log::HbLogService::processArgs( argc, argv );
 
     hb::linkexample::LinkMainWindow w;
--- network-example/network-user/src/main.cpp
+++ network-example/network-user/src/main.cpp
@@ -12,7 +12,7 @@ using hb::networkexample::UserMainWindow;
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    hb::tools::HbApplicationHelper::initApp("hb-company", "hb-company.com");
+    hb::tools::HbApplicationHelper::initApp( "hb-io", "hb-io.com" );
     hb::tools::HbApplicationHelper::initSkin("fusion");
 
     hb::log::HbLogService::processArgs( argc, argv );
