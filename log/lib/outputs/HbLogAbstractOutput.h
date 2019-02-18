46b64bef7ffd298a6aafc9c64a1b08a82c4fe772
Adrien Gavignet <adrien.gavignet@gmail.com>
2019-02-18 19:50:19 +0100

    Remove stupid license statement

--- log/lib/outputs/HbLogAbstractOutput.h
+++ log/lib/outputs/HbLogAbstractOutput.h
@@ -1,11 +1,3 @@
-/****************************************************************************
-** Copyright (C) 2012-2015 Adrien Gavignet (hasboeuf)
-** Contact: adrien.gavignet@gmail.com
-**
-** This file is part of the Hb module. Provided "AS IS", WITHOUT WARRANTIES
-** OR CONDITIONS OF ANY KIND, either express or implied.
-****************************************************************************/
-
 #ifndef HBLOGABSTRACTOUTPUT_H
 #define HBLOGABSTRACTOUTPUT_H
 
